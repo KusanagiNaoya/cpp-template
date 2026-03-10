a2aaa4e9100360a5847bec2629d6e0ac87fe5ea8
KusanagiNaoya <3063336434@qq.com>
2026-03-10 16:50:13 +0800

    first commit

---
+++ leetcode/editor/cn/two-sum.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ * @lcpr version=30300
+ *
+ * [1] 两数之和
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../common/ListNode.cpp"
+#include "../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return { i, j };
+                }
+            }
+        }
+        return {}; // 如果没有找到满足条件的组合，返回一个空数组
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// [2,7,11,15]\n9\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,4]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,3]\n6\n
+// @lcpr case=end
+
+ */
+
